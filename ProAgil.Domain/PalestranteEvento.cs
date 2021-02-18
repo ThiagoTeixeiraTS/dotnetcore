a42cf2d02bf3fd56172b9a546235f623a81878c8
ThiagoTeixeiraTS <thiagoemecal@gmail.com>
2021-02-18 16:44:17 -0300

    Organizar em Camadas

---
+++ ProAgil.Domain/PalestranteEvento.cs
@@ -0,0 +1,13 @@
+namespace ProAgil.Domain
+{
+    public class PalestranteEvento
+    {
+        public int PalestranteId { get; set; }
+
+        public Palestrante Palestrante { get;  }
+        
+        public int EventoId { get; set; }
+        
+        public Evento Evento { get;  }
+    }
+}
